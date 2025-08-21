5e52612c8020085defce012e6c0a6b7ec94cc139
GoldR0 <royx319@gmail.com>
2025-08-21 15:38:17 +0300

    managing media for better project structure

---
+++ campus-web-react/src/assets/index.ts
@@ -0,0 +1,74 @@
+// 🖼️ Assets Exports
+// קובץ זה מאפשר import קל של כל קבצי המדיה
+
+// לוגו ומיתוג
+export { default as onoLogo } from './onologo.png';
+
+// תמונות אירועים
+export { default as event1 } from './loc1.png';
+export { default as event2 } from './loc2.png';
+export { default as event3 } from './loc3.png';
+export { default as event4 } from './loc4.png';
+
+// תמונות קפיטריה
+export { default as cafeteria1 } from './a1.png';
+export { default as cafeteria2 } from './a2.png';
+export { default as cafeteria3 } from './a3.png';
+export { default as cafeteria4 } from './a4.png';
+export { default as cafeteria5 } from './a5.png';
+export { default as cafeteria6 } from './a6.png';
+export { default as cafeteria7 } from './a7.png';
+export { default as cafeteria8 } from './a8.png';
+export { default as cafeteria9 } from './a9.png';
+export { default as cafeteria10 } from './a10.png';
+export { default as cafeteria11 } from './a11.png';
+export { default as cafeteria12 } from './a12.png';
+export { default as cafeteria13 } from './a13.png';
+export { default as cafeteria14 } from './a14.png';
+export { default as cafeteria15 } from './a15.png';
+export { default as cafeteria16 } from './a16.png';
+export { default as cafeteria17 } from './a17.png';
+export { default as cafeteria18 } from './a18.png';
+export { default as cafeteria19 } from './a19.png';
+export { default as cafeteria20 } from './a20.png';
+export { default as cafeteria21 } from './a21.png';
+export { default as cafeteria22 } from './a22.png';
+export { default as cafeteria23 } from './a23.png';
+export { default as cafeteria24 } from './a24.png';
+
+// תמונות שירותים
+export { default as campusServices } from './שירותים בקמפוס (1).png';
+export { default as courseForum } from './פורום קורס (2).png';
+
+// תמונות נוספות (מספריות)
+export { default as image1 } from './1.png';
+export { default as image2 } from './2.png';
+export { default as image3 } from './3.png';
+export { default as image4 } from './4.png';
+export { default as image5 } from './5.png';
+export { default as image6 } from './6.png';
+export { default as image7 } from './7.png';
+export { default as image8 } from './8.png';
+export { default as image9 } from './9.png';
+export { default as image10 } from './10.png';
+export { default as image11 } from './11.png';
+export { default as image12 } from './12.png';
+export { default as image13 } from './13.png';
+export { default as image14 } from './14.png';
+export { default as image15 } from './15.png';
+export { default as image16 } from './16.png';
+export { default as image17 } from './17.png';
+export { default as image18 } from './18.png';
+export { default as image19 } from './19.png';
+export { default as image20 } from './20.png';
+export { default as image21 } from './21.png';
+export { default as image22 } from './22.png';
+export { default as image23 } from './23.png';
+export { default as image24 } from './24.png';
+export { default as image25 } from './25.png';
+export { default as image26 } from './26.png';
+export { default as image27 } from './27.png';
+export { default as image28 } from './28.png';
+export { default as image29 } from './29.png';
+export { default as image30 } from './30.png';
+export { default as image31 } from './31.png';
